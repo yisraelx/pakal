263a43ce9261fd077fc18c3b9c2c88f4f4b464aa
yisraelx <yisraelx@gmail.com>
2018-10-08 14:12:03 +0200

    feat(assertify): Throws custom error type, Returns some value.
    Added an option to throws a custom error type  if `fn` invokes returns
    `false`.
    Added an option to return some value instead of `void` if `fn`
    invokes returns `true`.
    
    BREAKING CHANGE: By default if `fn` invokes returns `true` it will
    returns `arguments[0]` instead of `void`.

--- modules/assertify/__tests__/index.spec.ts
+++ modules/assertify/__tests__/index.spec.ts
@@ -1,41 +1,96 @@
 import assertity from '../';
 
 describe(`assertity()`, () => {
-    it(`should assert and throw message`, () => {
-        let isString = (value) => typeof value === 'string';
-        let assertString = assertity(isString, 'The value is not string.');
-        expect(() => {
-            assertString(777);
-        }).toThrowError('The value is not string');
+  it(`should be a function`, () => {
+    expect(assertity).toBeFunction();
+  });
 
-        expect(assertString(':)')).toBeUndefined();
+  it(`should assert and throw message`, () => {
+    let isString = (value) => typeof value === 'string';
+    let assertString = assertity(isString, 'The value is not string.');
+    expect(() => {
+      assertString(777);
+    }).toThrowError('The value is not string');
+
+    expect(assertString(':)')).toBe(':)');
+  });
+
+  it(`should assert with params and throw fn message`, () => {
+    let toBe = (that: any, other: any) => that === other;
+    let assertToBe = assertity(toBe, (that: any, other: any) => {
+      return `${ that } !== ${ other }`;
     });
 
-    it(`should assert with params and throw fn message`, () => {
-        let toBe = (that: any, other: any) => that === other;
-        let assertToBe = assertity(toBe, (that: any, other: any) => {
-            return `${that} !== ${other}`;
-        });
+    expect(() => {
+      assertToBe(1, 2);
+    }).toThrowError('1 !== 2');
 
-        expect(() => {
-            assertToBe(1, 2);
-        }).toThrowError('1 !== 2');
+    expect(assertToBe(null, null)).toBeNull();
+  });
 
-        expect(assertToBe(null, null)).toBeUndefined();
-    });
+  it('should invokes `fn` with thisArs ', () => {
+    let object = {
+      num: 55,
+      isNum(num) {
+        return num === this.num;
+      },
+    };
 
-    it('should call on object scope', () => {
-        let object = {
-            num: 55,
-            isNum(num) {
-                return num === this.num;
-            }
-        };
+    object.isNum = assertity(object.isNum, 'is not num') as any;
+    expect(object.isNum(55)).toBe(55);
+    expect(() => {
+      object.isNum(33);
+    }).toThrowError('is not num');
+  });
 
-        object.isNum = assertity(object.isNum, 'is not num') as any;
-        expect(object.isNum(55)).toBeUndefined();
-        expect(() => {
-            object.isNum(33);
-        }).toThrowError('is not num');
-    });
+  it('should throw `message` string', () => {
+    expect(() => assertity((value) => value === 'cool', 'is not cool', false)('not cool')).toThrowError('is not cool');
+  });
+
+  it('should throw `message` object', () => {
+    expect(() => assertity((data) => data !== data, {message: 'cool'}, 0)('non')).toThrowError('cool');
+  });
+
+  it('should throw `message` fn invokes result string', () => {
+    expect(() => assertity((value) => value === 'cool', 'is not cool')('not cool')).toThrowError('is not cool');
+  });
+
+  it('should throw `message` fn invokes result object', () => {
+    expect(() => assertity(function(key) {
+      return key in this;
+    }, function(key) {
+      return `'${ key }' not in '${ this }'`;
+    }, true).call({
+      name: 'some',
+      toString() {
+        return `{ ${ this.name } }`;
+      },
+    }, 'foo')).toThrowError(`'foo' not in '{ some }'`);
+  });
+
+  it('should return by default args[0]', () => {
+    expect(assertity((num: number) => true, null)(55)).toBe(55);
+  });
+
+  it('should return args[0] if `returns` is true', () => {
+    expect(assertity((n: number, o: number) => n > o, null, true)(0.5, 0)).toBe(0.5);
+  });
+
+  it('should return void if `returns` is false ', () => {
+    expect(assertity((v) => v, null, false)(true)).toBeUndefined();
+  });
+
+  it('should return args[`returns`] if `returns` is number ', () => {
+    let object = {
+      ok: true,
+      fn: assertity(function(...args) {
+        return this.ok;
+      }, null, 1),
+    };
+    expect(object.fn('a', 'b')).toBe('b');
+  });
+
+  it('should return `returns` fn invokes result', () => {
+    expect(assertity((...args) => true, null, (a, b, c) => a + b + c)(1, 2, 3)).toBe(6);
+  });
 });
--- modules/assertify/index.ts
+++ modules/assertify/index.ts
@@ -1,15 +1,19 @@
-/**
+/*!
  * @module @pakal/assertify
- * @copyright © 2018 Yisrael Eliav <yisraelx@gmail.com> (https://github.com/yisraelx)
+ * @copyright © Yisrael Eliav <yisraelx@gmail.com> (https://github.com/yisraelx)
  * @license MIT
  */
+
 import arity from '@pakal/arity';
+import typeOf, { $function, $number, $object } from '@pakal/type-of';
 
 /**
- * @function
- * @category function
- * @param fn The function to assertify.
- * @param message The throw message or function that create message.
+ * Creates a wrapper function for `fn`, on invokes the wrapper it will invokes `fn` and throw if `fn` invokes result is `false`.
+ *
+ * @param fn - A validate function that returns boolean valid or not (if not it will throw `message`).
+ * @param message - A message to throw if `fn` invokes result `false`, can be message or fn that create message.
+ * @param returns - A value to returns if `fn` invokes result is `true`,
+ *  can be `false` to returns void or index to returns args[index] or fn that returns value to returns.
  * @returns Returns A assertified version of `fn`.
  * @example
  *
@@ -17,8 +21,8 @@ import arity from '@pakal/arity';
  *
  *  let assertFoo = assertify(isFoo, 'The value is not foo');
  *
- *  assertFoo('bar'); // => void
- *  assertFoo('some'); // => throw Error('The value is not foo')
+ *  assertFoo('bar'); // => 'bar'
+ *  assertFoo('some'); // throw Error('The value is not foo')
  *
  * @example
  *
@@ -26,24 +30,46 @@ import arity from '@pakal/arity';
  *
  *  let assertEquals = assertify(isEquals, (value: any, other: any) => {
  *      return `${value} !== ${other}`;
- *  });
+ *  }, false);
  *
  *  assertEquals(true, true); // => void
- *  assertEquals(0, 2); // => throw Error('0 !== 2')
+ *  assertEquals(0, 2); // throw Error('0 !== 2')
+ *
+ * @example
+ *
+ *  let isString = (value: any) => typeof value === 'string';
+ *  let assertString = assertify(isString, (value) => TypeError(`type of '${value}' is not string.`), 0);
+ *
+ *  assertString('some'); // => 'some'
+ *  assertString(true); // throw TypeError(`type of 'true' is not string`)
+ *
+ * @example
+ *
+ *  let contains = (items: any[], value: any) => items.indexOf(value) > -1;
+ *  let assertContains = assertify(contains, (items, value) => ({message: (`'${value}' not in ['${items.join(', ')}']`}), contains), 1);
+ *
+ *
+ *  assertContains(['red', 'black', 'green'], 'black'); // => 'black'
+ *  assertContains(['tea', 'beer'], 'coffee'); // throw {message: `'coffee' not in ['tea', 'beer']`}
+ *
  */
-function assertify<P1>(fn: (p1: P1) => boolean, message?: string | ((p1: P1) => string)): (p1: P1) => void;
-function assertify<P1, P2>(fn: (p1: P1, p2: P2) => boolean, message?: string | ((p1: P1, p2: P2) => string)): (p1: P1, p2: P2) => void;
-function assertify<P1, P2, P3>(fn: (p1: P1, p2: P2, p3: P3) => boolean, message?: string | ((p1: P1, p2: P2, p3: P3) => string)): (p1: P1, p2: P2, p3: P3) => void;
-function assertify<P1, P2, P3, P4>(fn: (p1: P1, p2: P2, p3: P3, p4: P4) => boolean, message?: string | ((p1: P1, p2: P2, p3: P3, p4: P4) => string)): (p1: P1, p2: P2, p3: P3, p4: P4) => void;
-function assertify<P1, P2, P3, P4, P5>(fn: (p1: P1, p2: P2, p3: P3, p4: P4, p5: P5) => boolean, message?: string | ((p1: P1, p2: P2, p3: P3, p4: P4, p5: P5) => string)): (p1: P1, p2: P2, p3: P3, p4: P4, p5: P5) => void;
-function assertify(fn: (...args: any[]) => boolean, message?: string | ((...args: any[]) => string)): (...args) => void {
-    return arity(function () {
-        let result = fn.apply(this, arguments);
-        if (!result) {
-            message = typeof message === 'function' ? message.apply(this, arguments) : message;
-            throw new Error(message as string);
-        }
-    }, fn.length);
+function assertify<TFn extends (...args: any[]) => boolean, TReturns extends boolean>(fn: TFn, message?: any | {message: any} | ((...args: Parameters<TFn>) => any | {message: any}), returns?: TReturns): (...args: Parameters<TFn>) => TReturns extends false ? void : Parameters<TFn>[0];
+function assertify<TFn extends (...args: any[]) => boolean, TArgIndex extends number>(fn: TFn, message?: any | {message: any} | ((...args: Parameters<TFn>) => any | {message: any}), returns?: TArgIndex): (...args: Parameters<TFn>) => Parameters<TFn>[TArgIndex];
+function assertify<TFn extends (...args: any[]) => boolean, TResult>(fn: TFn, message?: any | {message: any} | ((...args: Parameters<TFn>) => any | {message: any}), toResult?: (...args: Parameters<TFn>) => TResult): (...args: Parameters<TFn>) => TResult;
+function assertify<TReturnFn extends (...args: any[]) => any>(fn: (...args: Parameters<TReturnFn>) => boolean, message?: any | {message: any} | ((...args: Parameters<TReturnFn>) => any | {message: any}), toResult?: TReturnFn): TReturnFn;
+function assertify<TFn extends Function>(fn: TFn, message?: any | {message: any} | Function, returns?: number | boolean | Function): Function {
+  return arity(function(value) {
+    let result = fn.apply(this, arguments);
+
+    if (!result) {
+      message = typeOf(message) === $function ? message.apply(this, arguments) : message;
+      throw (typeOf(message) === $object && 'message' in message) ? message : Error(message);
+    }
+
+    if (returns !== false) {
+      return typeOf(returns as any) === $function ? (returns as Function).apply(this, arguments) : arguments[(typeOf(returns as any) === $number ? returns : 0) as number];
+    }
+  }, fn.length);
 }
 
 export default assertify;
