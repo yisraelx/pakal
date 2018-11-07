184eba87d4f081adece8ef8ef986ece4f128e9c5
yisraelx <yisraelx@gmail.com>
2018-11-07 16:07:36 +0200

    feat(forEach): `thisArg` option, break loop, support array like
    Added an option to pass `thisArg`.
    Added an option to break the loop by
    returning `false`.
    Added support for iterate on array like.
    
    BREAKING CHANGE: if `callback` invokes returns `false` it will break the
    loop.

--- modules/for-each/__tests__/index.spec.ts
+++ modules/for-each/__tests__/index.spec.ts
@@ -1,50 +1,146 @@
 import forEach from '../';
 
 describe(`forEach()`, () => {
-    it(`should be iteratee on array items`, () => {
-        let result = Array(3);
-        let array = ['a', 'b', 'c'];
-        forEach(array, (value, index, array) => {
-            result[index] = value;
-            expect(value).toBe(array[index]);
-        });
+  it('should be a function', () => {
+    expect(forEach).toBeFunction();
+  });
 
-        expect(result).toEqual(array);
+  it(`should be iteratee on array items`, () => {
+    let result = Array(3);
+    let array = ['a', 'b', 'c'];
+    forEach(array, (value, index, array) => {
+      result[index] = value;
+      expect(value).toBe(array[index]);
     });
 
-    it(`should be iteratee on object items`, () => {
-        let result = {};
-        let object = {a: 'A', b: 'B', c: 'C'};
-        forEach(object, (value, key, array) => {
-            result[key] = value;
-            expect(value).toBe(object[key]);
-        });
+    expect(result).toEqual(array);
+  });
 
-        expect(result).toEqual(object);
+  it('should be iterate on array like items', () => {
+    let arrayLike = {length: 3, 0: 'a', 1: 'b', 2: 'c', foo: 'bar'};
+    let value = '';
+    let key = 0;
+    expect(forEach(arrayLike, (v, k, a) => {
+      value += v;
+      key += k;
+
+      expect(value).toBeString();
+      expect(key).toBeNumber();
+      expect(a).toBe(arrayLike);
+    })).toBe(arrayLike);
+    expect(value).toBe('abc');
+    expect(key).toBe(3);
+  });
+
+  it('should be iterate on empty array like items', () => {
+    let amount: number = 3;
+    let arrayLike = {length: amount, foo: 'bar'};
+    expect(forEach(arrayLike, (value: undefined, index: number, {length}) => {
+      expect(value).toBeUndefined();
+      expect(amount + index).toBe(length);
+      amount--;
+    })).toBe(arrayLike);
+    expect(amount).toBe(0);
+  });
+
+  it(`should be iteratee on object properties`, () => {
+    let result = {};
+    let object = {a: 'A', b: 'B', c: 'C'};
+    forEach(object, (value, key, array) => {
+      result[key] = value;
+      expect(value).toBe(object[key]);
     });
 
-    it(`should be iteratee on chars`, () => {
-        let result = Array(5);
-        let word = 'hello';
-        forEach(word, (char, index, word) => {
-            result[index] = char;
-            expect(char).toBe(word[index]);
-        });
+    expect(result).toEqual(object);
+  });
 
-        expect(result.join('')).toBe(word);
+  it('should iteratee on function properties', () => {
+    let fn = Object.defineProperties(function(a, b, c) {
+    }, {foo: {value: 'bar', enumerable: true}, num: {value: 55}});
+    let result = {};
+    let count = 0;
+    expect(forEach(fn, (value, key, collection) => {
+      result[key] = value;
+      count++;
+      expect(collection).toBe(fn);
+    })).toBe(fn);
+    expect(result).toEqual({foo: 'bar'});
+    expect(count).toBe(1);
+  });
+
+  it(`should be iteratee on string chars`, () => {
+    let result = Array(5);
+    let word = 'hello';
+    forEach(word, (char, index, word) => {
+      result[index] = char;
+      expect(char).toBe(word[index]);
     });
 
-    it(`should be get non object value and return the value`, () => {
-        let result = forEach(1 as any, () => {});
+    expect(result.join('')).toBe(word);
+  });
 
-        expect(result).toBe(1);
+  it('should not iteratee on empty collection or non iteratee value and return `collection`', () => {
+    function callback() {
+      throw TypeError;
+    }
+
+    expect(forEach(null, callback)).toBeNull();
+    expect(forEach(undefined, callback)).toBeUndefined();
+    expect(forEach(true as any, callback)).toBeTruthy();
+    expect(forEach(false as any, callback)).toBeFalsy();
+    expect(forEach(NaN as any, callback)).toBeNaN();
+    expect(forEach(-1 as any, callback)).toBe(-1);
+    expect(forEach(0 as any, callback)).toBe(0);
+    expect(forEach(1 as any, callback)).toBe(1);
+    expect(forEach(1 as any, callback)).toBe(1);
+    expect(forEach([], callback)).toEqual([]);
+    expect(forEach('', callback)).toBe('');
+    expect(forEach({}, callback)).toEqual({});
+    expect(forEach(callback, callback)).toBe(callback);
+    expect(forEach(Object.create({foo: 'bar'}), callback)).toEqual(Object.create({foo: 'bar'}));
+    expect(forEach({length: 0}, callback)).toEqual({length: 0});
+  });
+
+  it('should iteratee with identity function if `callback` not define and return the collection', () => {
+    let count = 0;
+    let object = {
+      get some() {
+        count++;
+        return 'some';
+      },
+      get non() {
+        count++;
+        return false;
+      },
+      get other() {
+        count++;
+        return false;
+      },
+    };
+
+    expect(forEach(object)).toBe(object);
+    expect(count).toBe(2);
+  });
+
+  it('should break if iteratee function return false', () => {
+    let isOdd = (n) => n % 2 !== 0;
+    let array: number[] = [1, 3, 5, 6, 7, 9];
+    let count: number = 0;
+    forEach(array, (num: number, index: number) => {
+      count++;
+      return isOdd(num);
     });
+    expect(count).toBe(4);
+  });
 
-    it('should iteratee with identity function and return the collection', () => {
-        let object = {foo: 'bar'};
-        let result = forEach(object);
+  it('should invokes the callback with the given thisArg', () => {
+    let target = [2];
+    let result;
 
-        expect(result).toBe(object);
-    });
+    expect(forEach(target, function(v, k, t) {
+      result = this + v + t.length;
+    }, 3)).toBe(target);
 
+    expect(result).toBe(3 + 2 + 1);
+  });
 });
--- modules/for-each/index.ts
+++ modules/for-each/index.ts
@@ -1,24 +1,56 @@
-/**
+/*!
  * @module @pakal/for-each
- * @copyright © 2018 Yisrael Eliav <yisraelx@gmail.com> (https://github.com/yisraelx)
+ * @copyright © Yisrael Eliav <yisraelx@gmail.com> (https://github.com/yisraelx)
  * @license MIT
  */
+
+import isTypeOf from '@pakal/is-type-of';
 import keys from '@pakal/keys';
+import { $function, $undefined } from '@pakal/type-of';
 
 /**
- * @function
- * @category collection
+ * For each own enumerable properties of `collection` invokes `callback`.
+ *
+ * @param collection - A collection to iterate on it's properties.
+ * @param callback - A function to invokes for each property of `collection` (for `break` the loop return `false`).
+ * @param thisArg - A this arg to invokes with `callback`.
+ * @returns Returns the given `collection`.
  * @example
  *
- *  let array: string[] = ['a', 'b', 'c'];
+ *  let word: string = 'abc';
  *
- *  forEach(array, (value: string, index: number, array: string[]) => {
+ *  forEach(word, (char: string, index: number, array: string) => {
  *      console.log(`${index} = ${value}`);
  *  });
  *
  *  // => '0 = a'
  *  // => '1 = b'
  *  // => '2 = c'
+ *  // => 'abc'
+ *
+ * @example
+ *
+ *  let array: boolean[] = [true, true, false, true, true];
+ *
+ *  forEach(array, (value: boolean, index: number) => {
+ *      console.log(index);
+ *      return value;
+ *  });
+ *
+ *  // => 0
+ *  // => 1
+ *  // => 2
+ *  // => array
+ *
+ * @example
+ *
+ *  let array: ArrayLike<any> = {length: 2, 1: 'some', foo: 'bar'};
+ *
+ *  forEach(array, console.log);
+ *
+ *  // => undefined 0 array
+ *  // => 33 'some' array
+ *  // => array
  *
  * @example
  *
@@ -31,20 +63,52 @@ import keys from '@pakal/keys';
  *  // => 'a = 1'
  *  // => 'b = 2'
  *  // => 'c = 3'
+ *  // => object
+ *
+ * @example
+ *
+ *  forEach(); // => undefined
+ *  forEach(null); // => null
+ *  forEach(true, console.log); // => true
+ *
+ * @example
+ *
+ *  let _log = (value: any) => (console.log(value), value);
+ *  let collection = {
+ *   get foo(){ return _log('bar'); },
+ *   get non(){ return _log(false); },
+ *   get name(){ return _log('bob'); }
+ *  };
+ *
+ *  forEach(collection);
+ *
+ *  // => 'foo'
+ *  // => false
+ *  // => collection
+ *
  */
-function forEach<T extends ArrayLike<any>>(array: T, iteratee?: (value: T[keyof T & number], index: number, array: T) => void): T;
-function forEach<T extends object>(object: T, iteratee?: (value: T[keyof T], key: keyof T, object: T) => void): T;
-function forEach(collection: any, iteratee: Function = (v => v)) {
-    let objectKeys = !Array.isArray(collection) && keys(collection);
-    let {length} = objectKeys ? objectKeys : collection;
+function forEach<T extends ArrayLike<any>>(array?: T, callback?: (value: T[number], index: number, array: T) => any | boolean, thisArg?: any): T;
+function forEach<T extends object, K extends keyof T>(object: T, callback?: (value: T[K], key: K, object: T) => any | boolean, thisArg?: any): T;
+function forEach(collection?: any, callback?: Function, thisArg?: any): any {
+  callback = isTypeOf(callback, $undefined) ? v => v : callback;
 
-    for (let index: number = 0; index < length; index++) {
-        let key = objectKeys ? objectKeys[index] : index;
-        let value = collection[key];
-        iteratee(value, key, collection);
+  let length: number = collection && collection.length;
+  let objectKeys: string[];
+
+  if (isTypeOf(collection, $function) || +length !== length || length < 0) {
+    length = (objectKeys = keys(collection)).length;
+  }
+
+  for (let index: number = 0; index < length; index++) {
+    let key = objectKeys ? objectKeys[index] : index;
+    let value = collection[key];
+
+    if (callback.call(thisArg, value, key, collection) === false) {
+      break;
     }
+  }
 
-    return collection;
+  return collection;
 }
 
 export default forEach;
